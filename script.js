267b424c406cdd8882354aebcb5a2bdcd60b59b6
Sarunas Cironka <cironka.s@gmail.com>
2024-05-15 09:41:47 +0300

    final commit

--- script.js
+++ script.js
@@ -2,13 +2,31 @@ const points = document.getElementById("points");
 const gridContainer = document.querySelector(".gridContainer");
 let score = 0;
 
-//show green block randomly on a grid for 2s
+for (let i = 0; i < 25; i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  gridContainer.appendChild(cell);
+}
 
 function showBlock() {
+  const cells = document.querySelectorAll(".cell");
+  const randomIndex = Math.floor(Math.random() * cells.length);
   const block = document.createElement("div");
   block.classList.add("block");
-  gridContainer.appendChild(block);
-  setTimeout(() => {
+
+  block.addEventListener("click", () => {
+    score++;
+    points.textContent = `Score: ${score}`;
     block.remove();
-  }, 2000);
+  });
+
+  cells[randomIndex].appendChild(block);
+
+  setTimeout(() => {
+    if (block.parentElement) {
+      block.remove();
+    }
+  }, 500);
 }
+
+setInterval(showBlock, 500);
